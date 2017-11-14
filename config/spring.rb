49d85dcc228861e8004799f7491bc8ea916bda0d
Hery Ramihajamalala <hramihaj@redhat.com>
2017-11-14 19:25:12 +0100

    rake rails:update
    Updating configuration to match with rails 5.0
    
    TODO preview

--- config/spring.rb
+++ config/spring.rb
@@ -2,3 +2,10 @@ Spring.after_fork do
   FactoryGirl.reload if defined?(FactoryGirl)
   MessageBus.after_fork if defined?(MessageBus)
 end
+
+%w(
+  .ruby-version
+  .rbenv-vars
+  tmp/restart.txt
+  tmp/caching-dev.txt
+).each { |path| Spring.watch(path) }
