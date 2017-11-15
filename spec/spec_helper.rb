03ea2c110b5422f1e5a3e094cd6171746d16c2d9
Hery Ramihajamalala <hramihaj@redhat.com>
2017-11-15 23:36:19 +0100

    Use use_transaction_tests instead of use_transactional_fixtures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,7 +39,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_tests = true
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -54,15 +54,15 @@ RSpec.configure do |config|
 
   config.around(:each, transactions: false) do |ex|
     require 'database_cleaner'
-    transactional = self.use_transactional_fixtures
-    self.use_transactional_fixtures = false
+    transactional = self.use_transactional_tests
+    self.use_transactional_tests = false
 
     begin
       DatabaseCleaner.strategy = :transaction
       ex.run
       DatabaseCleaner.clean_with(:deletion)
     ensure
-      self.use_transactional_fixtures = transactional
+      self.use_transactional_tests = transactional
     end
   end
 
--- test/test_helpers/transactional_fixtures.rb
+++ test/test_helpers/transactional_fixtures.rb
@@ -4,7 +4,7 @@ module TestHelpers
   module TransactionalFixtures
     def disable_transactional_fixtures!
 
-      self.use_transactional_fixtures = false
+      self.use_transactional_tests = false
 
       self.teardown do
         DatabaseCleaner.clean_with :deletion
