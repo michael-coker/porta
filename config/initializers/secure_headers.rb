b662c2a2a6409387f0e6a07a6baa816a5197376b
hallelujah <hramihaj@redhat.com>
2018-01-13 18:47:17 +0100

    Revert "[gem] Update secure_headers to 5.0"
    This reverts commit c138186b25ec8d36b60dfbe1dc320630d5a06fbd.

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,12 +1,12 @@
-::SecureHeaders::Configuration.default do |config|
-  config.hsts = SecureHeaders::OPT_OUT
+::SecureHeaders::Configuration.configure do |config|
+  config.hsts = false
   config.x_frame_options = 'DENY'
   config.x_content_type_options = 'nosniff'
-  config.x_xss_protection = '1; mode=block'
+  config.x_xss_protection = { value: 1, mode: 'block' }
 
-  config.csp = SecureHeaders::OPT_OUT
+  config.csp = false
 
   # not sure what they do, so rather disabling them
-  config.x_download_options = SecureHeaders::OPT_OUT
-  config.x_permitted_cross_domain_policies = SecureHeaders::OPT_OUT
+  config.x_download_options = false
+  config.x_permitted_cross_domain_policies = false
 end
