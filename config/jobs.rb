bf2534370a00b0d972a4356f02cfcd1a80a598ed
Marta Noya <mnoyabon@redhat.com>
2018-09-19 12:33:47 +0200

    Run jobs (to change state of account 'activity') weekly

--- config/jobs.rb
+++ config/jobs.rb
@@ -22,6 +22,8 @@ module ThreeScale
     WEEK = %w[
       Pdf::Dispatch.weekly
       JanitorWorker.perform_async
+      FindAndScheduleForDeletionSuspendedAccountsWorker.perform_async
+      FindAndSuspendInactiveAccountsWorker.perform_async
     ].freeze
 
     DAILY = %w[
